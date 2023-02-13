92100200c3afa12a239bc7c577b41ddc3203b034
Daniel Tremura <tremuradaniel@gmail.com>
2023-02-13 20:33:21 +0200

    auto generate map IDs

--- pet-clinic-web/src/main/java/app/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/app/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("M");
         owner1.setLastName("D");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("MD");
         owner2.setLastName("DD");
 
@@ -39,7 +37,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println(ownerService.findAll().size());
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("D");
         vet1.setLastName("D");
 
